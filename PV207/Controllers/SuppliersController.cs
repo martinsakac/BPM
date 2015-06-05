138121f18a6bade65c76c158088a8f0100a229f5
martinsakac <sakac.m@gmail.com>
2015-06-05 23:18:24 +0200

    Adding new controllers

---
+++ PV207/Controllers/SuppliersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PV207.Controllers
+{
+    public class SuppliersController : ApiController
+    {
+        [HttpGet]
+        [Route("api/suppliers")]
+        public IHttpActionResult VerifyOrder(string productName, int price, int amount)
+        {
+            Random rnd = new Random();
+            int value = rnd.Next(2);
+
+            if (value == 0)
+            {
+                return Ok(true);
+            }
+            else
+            {
+                return Ok(false);
+            }
+        }
+    }
+}
---
+++ PV207/Controllers/TransactionsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PV207.Models;
+
+namespace PV207.Controllers
+{
+    public class TransactionsController : ApiController
+    {
+        
+        [HttpGet]
+        [Route("api/transactions/verify")]
+        public IHttpActionResult VerifyTransaction()
+        {
+            Random rnd = new Random();
+            int value = rnd.Next(3);
+
+            if (value == 0 || value == 1)
+            {
+                return Ok(true);
+            }
+            else
+            {
+                return Ok(false);
+            }
+        }
+
+    }
+}
